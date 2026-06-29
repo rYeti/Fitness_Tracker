58c0c6c3e5e2e003c7b306bb6eb662ab47e6ecf8
roberts <robert.stertz1@hs-augsburg.de>
2026-06-29 14:55:24 +0200

    chore: update versioning in pubspec files and adjust dependencies

--- fittnes_tracker/android/app/build.gradle.kts
+++ fittnes_tracker/android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.forgeform.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     compileOptions {
